6a99597af1509368d4b1f339f0c8d842fa93eb81
mrehanamjad <rehanamjad814@gmail.com>
2024-05-10 17:13:29 +0500

    context api project

---
+++ 08_Context-API_Project-1/src/context/UserContextProvider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import UserContext from "./userContext";
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return(
+        <UserContext.Provider value={{user,setUser}}> //use can give more values as you want
+            {children}
+        </UserContext.Provider>
+    )
+
+}
+
+
+export default UserContextProvider;
+
