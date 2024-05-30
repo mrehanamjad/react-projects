b6c067868c88987c3ea865ace266ad30ea0bdb08
“mrehanamjad” <rehanamjad814@gmail.com>
2024-05-30 09:49:50 -0700

    added appwrite- authentication  auth.js

---
+++ megaBlog-appwrite/src/appwrite/auth.js
@@ -0,0 +1,70 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+// better code practice
+// we not axactly copy past according to documentation because we make it industrial standard with best practices so that we can use services(or our own backend) other than appwrite in future if we want
+
+export class AuthService {
+  client = new Client();
+    // if want to use other services rathar than appwrite if future ==> change the constructor
+  constructor() {
+    this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client)
+  }
+
+  async CreateAcount(email,password,name){
+    try {
+    // if want to use other services rathar than appwrite if future ==> change below
+        const userAccount = await this.account.create(ID.unique(),email,password,name)
+        if (userAccount) {
+            //call another method
+            this.login({email,password})
+        } else {
+            return userAccount
+        }
+    } catch (error) {
+     throw error   
+    }
+  }
+
+  async login(email,password){
+    try {
+        return await account.createEmailPasswordSession(email, password);
+    } catch (error) {
+        throw error
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+        return await this.account.get();
+        // Logged in
+    } catch (error) {
+        console.log("Appwrite service :: getCurrentUser :: error",error);
+    }
+    
+    return null // we can also use if-else to chech if account.get()
+  }
+
+  async logout() {
+    try {
+        return await account.deleteSessions();
+    } catch (error) {
+        console.log("Appwrite service :: logout :: error",error);
+    }
+  }
+
+}
+
+const authService = new AuthService(); // authService is object while  AuthService is class
+
+
+/* if we export AuthService (class) then every time we have to create an object and use it
+therefore we have created an object and export it */
+export default authService;
+
+
+
+
--- megaBlog-appwrite/src/conf/conf.js
+++ megaBlog-appwrite/src/conf/conf.js
@@ -3,10 +3,10 @@
 
 const conf = {
     appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
 
 export default conf
